485f1c20d39cfeb50586100827f8f2c373222059
Yuri D <breakeneck@gmail.com>
2020-05-11 14:02:33 +0000

    implemented shlokas parsing

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ const BOOK = 'SB';
 let $ = null;
 
 async function getPage(uri = '') {
-    let url = uri ? `${URL}${uri}` : `${URL}/${LANG}/${BOOK}`;
+    let url = uri ? (uri.indexOf('http://') > -1 ? uri : `${URL}${uri}`) : `${URL}/${LANG}/${BOOK}`;
     let page = await axios.get(url);
     return cheerio.load(page.data);
 }
@@ -41,6 +41,32 @@ async function getLinks(url = '', regexp = /(.*)/) {
     return items;
 }
 
+async function parseShloka(uri) {
+    let $ = await getPage(uri);
+    let sanskrit = $('blockquote').text().trim();
+    return {
+        uri: uri,
+        sanskrit: sanskrit,
+        wordly: sanskrit
+            ? $('blockquote').closest('.row').next().find('.dia_text p').text().trim()
+            : '',
+        text: $('.col-md-12 h4').text().trim(),
+        comment: sanskrit
+             ? $('.dia_text .dia_text').text().trim()
+             : $('.pager').next().find('.dia_text').text().trim()
+    };
+}
+
+async function getShlokas(links) {
+    let result = [];
+    for (let link of links) {
+        let shloka = await parseShloka(link.url);
+        result.push(shloka);
+    }
+    return result;
+    // return await links.map(async (link) => await parseShloka(link.url))
+}
+
 
 (async () => {
     let cantos = await getLinks('', /«(.*)»/);
@@ -48,7 +74,8 @@ async function getLinks(url = '', regexp = /(.*)/) {
     for (let canto of cantos) {
         canto.chapters = await getLinks(canto.url, /\d*(.*)/);
         for (let chapter of canto.chapters) {
-            chapter.shlokas = await getLinks(chapter.url);
+            let links = await getLinks(chapter.url);
+            chapter.shlokas = await getShlokas(links);
         }
     }
 
