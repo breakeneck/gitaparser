4840890f52bea0962f99e91d7f2bd9a2f4cf0bfc
Yuri Datsenko <breakeneck@gmail.com>
2022-06-07 13:23:30 +0300

    Implemented parsing by config

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const Parser = require("./src/parser")
+const fs = require('fs');
 
 const STAGES = {
     init: {title: 'init database (init engine)', argsCount: 2},
     chapters: {title: 'parse book structure (engine chapters url abbr lang)', argsCount: 5},
     content: {title: 'parse texts (content engine book_id)', argsCount: 3},
+    file: {title: 'parse json file (file filename)', argsCount: 2},
     delete: {title: 'delete book (delete engine book_id)', argsCount: 3}
 };
 const [STAGE] = process.argv.slice(2, 3);
@@ -38,6 +40,16 @@ function loadArgs() {
             for (let [i, book_id] of book_ids.split(',').entries()) {
                 await parser.loadBook(book_id);
                 await parser.bookContent(i === 0 ? last_chapter_id : 0);
+            }
+            break;
+
+        case 'file':
+            let entries = JSON.parse(fs.readFileSync(args.shift(), 'utf8'))
+            for (let params of entries) {
+                let book_id = await parser.newBook(params.url, params.abbr, params.lang, params.levels);
+                await parser.bookStructure();
+                console.log(`Created book ${book_id}. Parsing content...`);
+                await parser.bookContent();
             }
             break;
 
